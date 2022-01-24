133a04c4666371e18c413ee33a2479f735d61297
luizsaulo <luizsaulo@yahoo.com.br>
2022-01-24 11:01:54 -0300

    implementação de filtros por distância e preço máximo

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-function Card() {
+function Card({ name, price, userName, userWhats }) {
     
     return (
         <div className='card'>                    
-            <h2>Campanha de Natal</h2>
-            <h1>R$ 20.000,00</h1>
+            <h2>{ name }</h2>
+            <h1>R$ { price }</h1>
             <div className='card-info'>
                 <div className='card-name'>
                     <img src='' alt='' />
-                    <p>Bloguerio X</p>
+                    <p>{ userName }</p>
                 </div>
                 <img src='' alt='' />
-                <p>+55 11 99999-9999</p>
+                <p>{ userWhats }</p>
             </div>
         </div>
     )
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import Card from '../../Components/Card/Card';
 import Modal from '../../Components/Modal/Modal';
+import api from '../../Services/Api';
 
 
 function Home() {
     const [isModalOpen, setMdoalOpen] = useState(false)
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [productsData, setProductsData] = useState([])
+    const [productsByName, setProductsByName] = useState('')
+    const [productsByMaxPrice, setProductsByMaxPrice] = useState(500000000)
+
+    useEffect(() => {
+        getUserLocation()
+    }, [])
+
+    async function getUserLocation() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords
+            setLatitude(latitude)
+            setLongitude(longitude)
+        }, (err) => {
+            console.log(err)
+        }, { timeout: 10000 })
+            
+    }
+
+    useEffect(() => {
+        getNearByProducts()
+    }, [latitude, longitude])
+
+    async function getNearByProducts() {
+        try {
+            const nearByProducts = await api.get(`/product?latitude=${latitude}&longitude=${longitude}`)
+            const { data } = nearByProducts
+            setProductsData(data)
+        } catch(err) {
+            alert('Erro ao carregar os itens')
+        }
+    }
+
+    useEffect(() => {
+        getFilteredProducts()
+    }, [productsByName, productsByMaxPrice])
+
+    
 
     function openModal() {
         setMdoalOpen(true)
@@ -22,15 +63,33 @@ function Home() {
                 <form>
                     <h1>Pesquisar por campanha</h1>
                     <div className='form-inputs'>
-                        <input type='text' placeholder='pesquisar por camapanha' />
-                        <input type='number' min='0' placeholder='preço máximo' />
+                        <input 
+                            type='text' 
+                            placeholder='pesquisar por camapanha' 
+                            value={productsByName}
+                            onChange={e => setProductsByName(e.target.value)}
+                        />
+                        <input 
+                            type='number' 
+                            min='0' 
+                            placeholder='preço máximo' 
+                            value={productsByMaxPrice}
+                            onChange={e => setProductsByMaxPrice(e.target.value)}
+                        />
                     </div>
                 </form>
             </section>
             <section className='products-section'>
-                <section className='products-container'>                    
-                    <Card />
-                    <Card />                   
+                <section className='products-container'> 
+                    {productsData.map(product => (
+                        <Card key={product._id}
+                            name={product.name}
+                            price={product.price}
+                            userName={product.user.name}
+                            userWhats={product.user.whatsapp}
+                        />
+                    ))}                   
+                                       
                 </section>
             </section>
 
