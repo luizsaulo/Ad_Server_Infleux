8af6f32fb767812380d8e71621fee648f1d8883e
luizsaulo <luizsaulo@yahoo.com.br>
2022-01-24 00:44:18 -0300

    implementação da criação de usuário no front integrando com o back

--- src/Components/RegisterModal/RegisterModal.js
+++ src/Components/RegisterModal/RegisterModal.js
@@ -1,16 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import api from '../../Services/Api'
+
+
 
 function RegisterModal({ setLoginForm }) {
+    const [name, setName] = useState('')
+    const [whatsapp, setWhatsapp] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+
+    
+
+    useEffect(() => {
+        getUserLocation()
+    }, [])
+
+    async function registrationHandler(e) {
+        e.preventDefault()
+        try {
+            await api.post('user', {
+                name,
+                whatsapp,
+                email,
+                password,
+                latitude,
+                longitude             
+            })
+            alert('Cadastro relizado com sucesso!')           
+        } catch(err) {
+            alert('Erro ao cadastrar usuário, tente novamente')
+        }
+    }
+
+    async function getUserLocation() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords
+            setLatitude(latitude)
+            setLongitude(longitude)
+        }, (err) => {
+            console.log(err)
+        }, { timeout: 10000 })
+            
+    }
+
     return(
         <div className='modal'>
                     <h1>Cadastrar</h1>
                     <form action=''>
-                        <input type='text' placeholder='nome' />
-                        <input type='text' placeholder='whatsapp' />
-                        <input type='text' placeholder='email' />
-                        <input type='text' placeholder='senha' />
-                        <button>Cadastrar</button>
+                        <input 
+                            type='text' 
+                            placeholder='nome'
+                            value={name}
+                            onChange={e=>setName(e.target.value)}
+                        />
+                        <input 
+                            type='text' 
+                            placeholder='whatsapp'
+                            value={whatsapp}
+                            onChange={e=>setWhatsapp(e.target.value)}
+                        />
+                        <input 
+                            type='text' 
+                            placeholder='email'
+                            value={email}
+                            onChange={e=>setEmail(e.target.value)}
+                        />
+                        <input 
+                            type='text' 
+                            placeholder='senha'
+                            value={password}
+                            onChange={e=>setPassword(e.target.value)}
+                        />
+                        <button onClick={registrationHandler}>Cadastrar</button>
                         <Link onClick={setLoginForm}>Já possuo Conta</Link>
                     </form>
         </div>
